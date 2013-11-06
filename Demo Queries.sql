4371982b552263a9bdeeda28f841f7e3290db947
soncu <zigana_m16@windowslive.com>
2013-11-06 09:29:57 +0200

    fixed a typo

--- Demo Queries.sql
+++ Demo Queries.sql
@@ -7,7 +7,7 @@ SELECT dbo.MyListGetAt('-one-two-three-four-five', -6, '-', 0)-- ''
 SELECT dbo.MyListGetAt('1,2,,,4,5', 3, default, default)-- 4
 SELECT dbo.MyListGetAt('1,2,,,4,5', 4, default, 0)-- 1,2,,*,4,5
 SELECT dbo.MyListGetAt('1,2,,,4,5', -5, default, 0)-- 2
-SELECT dbo.MyListGetAt(';This is,- just; a-, test; case-', 4, default, 0)-- ' test; case-'
+SELECT dbo.MyListGetAt(';This is,- just; a-, test; case-', 3, default, 0)-- ' test; case-'
 SELECT dbo.MyListGetAt(';This is,- just; a-, test; case-', 4, ';,-', 0)-- ' just'
 SELECT dbo.MyListGetAt(';This is,- just; a-, test; case-', -5, ';,-', 0)-- ' a'
 SELECT dbo.MyListGetAt(';This is,- just; a-, test; case-', -5, ';,-', default)-- 'This is'
